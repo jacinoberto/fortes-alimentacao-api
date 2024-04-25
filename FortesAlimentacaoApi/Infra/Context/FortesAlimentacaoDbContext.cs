569c83a43b285bb81f8b4752b6813bff29d0ddaa
Jacimara Noberto <126730453+devjacinoberto@users.noreply.github.com>
2024-04-25 19:38:12 -0300

    Conexão com banco estabelecida

---
+++ FortesAlimentacaoApi/Infra/Context/FortesAlimentacaoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FortesAlimentacaoApi.Infra.Context;
+
+public class FortesAlimentacaoDbContext : DbContext
+{
+    private IConfiguration _configuration;
+
+    public FortesAlimentacaoDbContext(IConfiguration configuration, DbContextOptions options)
+        : base(options)
+    {
+        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = _configuration.GetConnectionString("FortesAlimentacaoConnection");
+        optionsBuilder.UseNpgsql(connectionString);
+    }
+}
--- FortesAlimentacaoApi/Program.cs
+++ FortesAlimentacaoApi/Program.cs
@@ -1,3 +1,5 @@
+using FortesAlimentacaoApi.Infra.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add context
+builder.Services.AddDbContext<FortesAlimentacaoDbContext>();
 
 var app = builder.Build();
 
